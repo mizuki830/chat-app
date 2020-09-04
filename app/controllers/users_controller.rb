402f3cb2ff227a4cf0c1da6fc89b6019f9729e16
mizuki830 <mizuki.lovebaske.0830@ezweb.ne.jp>
2020-09-04 15:24:03 +0900

    ユーザー情報表示の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,9 @@ class UsersController < ApplicationController
   def update
     if current_user.update(user_params)
       redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
